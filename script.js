0fa7dff19205ffaeb987676150e43ac9bf4d9ad2
Van-13 <nessa.herpich@yahoo.com.br>
2022-01-04 14:56:38 -0300

    adicionando api

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ const fetchGatos = ()=> {
 }
 
 fetchGatos()
+
+
+const botaodoacao= document.querySelector(".formulario") 
+botaodoacao.addEventListener("submit", (evento)=>{
+    console.log(evento)
+    evento.preventDefault()
+    doar(evento)
+
+})
+
+
+
+function doar(evento){
+    evento.preventDefault()
+    const escolha=document.querySelector(".escolha").value
+    console.log(escolha)
+    const objetoescolha= {
+        escolhaopcoes: escolha
+    }
+    fetch("https://prog-bolsas-api.herokuapp.com/",{
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(objetoescolha)
+
+    })
+    .then(responde=>responde.json())
+    .then(opcao=>console.log(opcao))
+}
+
